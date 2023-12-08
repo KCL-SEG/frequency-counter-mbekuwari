33d9366e530ba09a12f04cc96ca707cb0e233f2c
Mohammed Alkuwari <Mohdalkuwari7@gmail.com>
2023-12-08 01:21:12 +0000

    added code

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
+
+    for item in items:
+
+        value = str(item)
+        if value in frequencies:
+            frequencies[value] += 1
+        else:
+            frequencies[value] = 1
+
     # Your code goes here
     return frequencies
